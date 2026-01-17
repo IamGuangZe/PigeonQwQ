b3fdfbd754bee6baa1cb78de0a62014990c2bb36
IamGuangZe <kstvGuangze@gmail.com>
2026-01-17 20:45:21 +0800

    fix: 修复 isPressed 无法检测鼠标按键 - 通过 GLFW 处理鼠标按键绑定 - 添加 onPressed 辅助方法

--- src/main/java/owo/pigeon/utils/KeybindUtil.java
+++ src/main/java/owo/pigeon/utils/KeybindUtil.java
@@ -2,11 +2,16 @@ package owo.pigeon.utils;
 
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import owo.pigeon.mixin.accessors.IAccessorKeyBinding;
 
 import static owo.pigeon.Pigeonqwq.mc;
 
 public class KeybindUtil {
+    public static void onPressed(KeyBinding key) {
+        KeyBinding.onKeyPressed(((IAccessorKeyBinding) key).getBoundKey());
+    }
+
     public static void setPressed(KeyBinding key, boolean pressed) {
         if (key.isUnbound()) return;
         key.setPressed(pressed);
@@ -14,6 +19,12 @@ public class KeybindUtil {
 
     public static boolean isPressed(KeyBinding key) {
         if (mc.getWindow() == null || key.isUnbound()) return false;
-        return InputUtil.isKeyPressed(mc.getWindow(),((IAccessorKeyBinding) key).getBoundKey().getCode());
+
+        InputUtil.Key boundKey = ((IAccessorKeyBinding) key).getBoundKey();
+        if (boundKey.getCategory() == InputUtil.Type.MOUSE) {
+            return GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), boundKey.getCode()) == GLFW.GLFW_PRESS;
+        } else {
+            return InputUtil.isKeyPressed(mc.getWindow(), boundKey.getCode());
+        }
     }
 }
