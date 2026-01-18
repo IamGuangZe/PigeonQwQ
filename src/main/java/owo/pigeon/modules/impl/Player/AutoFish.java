285e0568fd6214b2b8ef2885ff0e71f8032e1c3b
IamGuangZe <kstvGuangze@gmail.com>
2026-01-18 21:50:13 +0800

    feat: 为 AutoFish 添加设置 stop-in-gui - 使用 mc.player.fishHook 来检测浮标

--- src/main/java/owo/pigeon/modules/impl/Player/AutoFish.java
+++ src/main/java/owo/pigeon/modules/impl/Player/AutoFish.java
@@ -2,7 +2,6 @@ package owo.pigeon.modules.impl.Player;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
-import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.FishingRodItem;
 import net.minecraft.item.ItemStack;
 import owo.pigeon.modules.Category;
@@ -11,24 +10,21 @@ import owo.pigeon.settings.EnableSetting;
 import owo.pigeon.settings.IntSetting;
 import owo.pigeon.settings.ModeSetting;
 import owo.pigeon.utils.Chat.ChatUtil;
-import owo.pigeon.utils.ColorUtil;
 import owo.pigeon.utils.PlayerUtil;
 import owo.pigeon.utils.WorldUtil;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static owo.pigeon.Pigeonqwq.mc;
 
 public class AutoFish extends Module {
     public AutoFish() {
-        super("AutoFish",Category.PLAYER);
+        super("AutoFish", Category.PLAYER);
     }
 
-    public ModeSetting<PlayerUtil.RightClickMode> castMode = setting("cast-mode", PlayerUtil.RightClickMode.MOUSE,v->true);
-    public EnableSetting slugfishMode = setting("slugfish-mode", false, v -> true);
+    public ModeSetting<PlayerUtil.RightClickMode> castMode = setting("cast-mode", PlayerUtil.RightClickMode.MOUSE, v -> true);
+    public EnableSetting stopInGui = setting("stop-in-gui", true, v -> true);
     public EnableSetting rethrow = setting("rethrow", true, v -> true);
     public IntSetting rethrowTick = setting("rethrow-tick", 10, 1, 20, v -> rethrow.getValue());
+    public EnableSetting slugfishMode = setting("slugfish-mode", false, v -> true);
 
     private int rethrowTicks;
     private boolean fishIncoming;
@@ -41,56 +37,40 @@ public class AutoFish extends Module {
     @Override
     public void onTickPost() {
         if (WorldUtil.nullCheck()) return;
+        if (stopInGui.getValue() && mc.currentScreen != null) return;
 
-        List<FishingBobberEntity> fishingBobbers = new ArrayList<>();
-        List<ArmorStandEntity> armorStands = new ArrayList<>();
-        FishingBobberEntity playerBobber = null;
-
-        for (Entity entity : mc.world.getEntities()) {
-            if (entity instanceof FishingBobberEntity fishingBobber) {
-                fishingBobbers.add(fishingBobber);
-            } else if (entity instanceof ArmorStandEntity armorStand) {
-                armorStands.add(armorStand);
-            }
-        }
-
-        // Player's Bobber
-        for (FishingBobberEntity fishingBobber : fishingBobbers) {
-            if (fishingBobber.getOwner() == mc.player) {
-                playerBobber = fishingBobber;
-            }
-        }
-
-        int bobberAge;
-        if (playerBobber != null) bobberAge = playerBobber.age;
-        else bobberAge = 0;
+        // fishHookAge
+        int fishHookAge;
+        if (mc.player.fishHook != null) fishHookAge = mc.player.fishHook.age;
+        else fishHookAge = 0;
 
         // Rethrow
-        if (playerBobber == null && rethrowTicks < rethrowTick.getValue() + 1) rethrowTicks ++;
+        if (mc.player.fishHook == null && rethrowTicks < rethrowTick.getValue() + 1) rethrowTicks++;
         if (isHeldRod() && rethrowTicks == rethrowTick.getValue()) PlayerUtil.RightClick(castMode.getValue());
 
         // Catch Fish
-        for (ArmorStandEntity armorStand : armorStands) {
-            String name = ColorUtil.removeColorA(armorStand.getDisplayName().getString());
+        for (Entity entity : mc.world.getEntities()) {
+            if (entity instanceof ArmorStandEntity armorStand) {
+                String name = armorStand.getDisplayName().getString();
 
-            if (name.equals("?") || name.matches("\\d\\.\\d")) {
-                fishIncoming = true;
-            }
+                if (name.equals("?") || name.matches("\\d\\.\\d")) {
+                    fishIncoming = true;
+                }
 
-            if (name.equals("!!!") && fishIncoming) {
-                fishIncoming = false;
+                if (name.equals("!!!") && fishIncoming) {
+                    fishIncoming = false;
 
-                if (!slugfishMode.getValue() || bobberAge > 22 * 20) {
-                    PlayerUtil.RightClick(castMode.getValue());
-                    rethrowTicks = 0;
+                    if (!slugfishMode.getValue() || fishHookAge > 22 * 20) {
+                        PlayerUtil.RightClick(castMode.getValue());
+                        rethrowTicks = 0;
+                    }
                 }
             }
         }
 
-        if (fishingBobbers.isEmpty()) ChatUtil.sendDebugMessage(this.name,"No fishing bobbers in the world.");
-        else if (playerBobber == null) ChatUtil.sendDebugMessage(this.name,"Player's bobber not found. Total bobbers: " + fishingBobbers.size());
-        else if (!fishIncoming) ChatUtil.sendDebugMessage(this.name,"Fish not incoming.");
-        else ChatUtil.sendDebugMessage(this.name,"Waiting to catch.");
+        if (mc.player.fishHook == null) ChatUtil.sendDebugMessage(this.name, "Player's bobber not found.");
+        else if (!fishIncoming) ChatUtil.sendDebugMessage(this.name, "Fish not incoming.");
+        else ChatUtil.sendDebugMessage(this.name, "Waiting to catch.");
     }
 
     private boolean isHeldRod() {
