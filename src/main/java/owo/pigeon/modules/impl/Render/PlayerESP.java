1b1de87e563559b163bfeb9a277df1d22b1a1726
IamGuangZe <kstvGuangze@gmail.com>
2026-02-06 02:04:31 +0800

    feat: 添加 PlayerESP 模块

---
+++ src/main/java/owo/pigeon/modules/impl/Render/PlayerESP.java
@@ -0,0 +1,32 @@
+package owo.pigeon.modules.impl.Render;
+
+import net.engio.mbassy.listener.Handler;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import owo.pigeon.event.events.RenderEvent;
+import owo.pigeon.modules.Category;
+import owo.pigeon.modules.Module;
+import owo.pigeon.settings.ColorSetting;
+import owo.pigeon.settings.ModeSetting;
+import owo.pigeon.utils.PlayerUtil;
+import owo.pigeon.utils.Render.RenderUtil;
+
+import java.awt.*;
+
+import static owo.pigeon.Pigeonqwq.mc;
+
+public class PlayerESP extends Module {
+    public PlayerESP() {
+        super("PlayerESP", Category.RENDER);
+    }
+
+    public ModeSetting<RenderUtil.ESPMode> mode = setting("mode", RenderUtil.ESPMode.FILL, v -> true);
+    public ColorSetting color = setting("color", new Color(0x5FFFFFFF, true), v -> true);
+
+    @Handler
+    public void onRender3D(RenderEvent.Render3DEvent event) {
+        for (AbstractClientPlayerEntity player : mc.world.getPlayers()) {
+            if (player != mc.player && PlayerUtil.hasUUID(player))
+                RenderUtil.drawESP(event.getMatrix(),player,color.getValue(),mode.getValue());
+        }
+    }
+}
