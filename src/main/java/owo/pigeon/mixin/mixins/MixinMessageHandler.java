d254d129b411db0e84dbf0eb005a66e09523c1f2
IamGuangZe <kstvGuangze@gmail.com>
2026-02-08 07:04:59 +0800

    chore: 禁用 MessageHandler 中的冗余调试日志

--- src/main/java/owo/pigeon/mixin/mixins/MixinMessageHandler.java
+++ src/main/java/owo/pigeon/mixin/mixins/MixinMessageHandler.java
@@ -8,13 +8,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import owo.pigeon.Pigeonqwq;
 import owo.pigeon.event.events.MessageEvent;
-import owo.pigeon.utils.Chat.ChatUtil;
 
 @Mixin(MessageHandler.class)
 public class MixinMessageHandler {
     @Inject(method = "onGameMessage", at = @At("HEAD"), cancellable = true)
     public void onGameMessage(Text message, boolean overlay, CallbackInfo ci) {
-        ChatUtil.sendDebugMessage("MessageHandler","Game Message: " + message.getString());
+        // ChatUtil.sendDebugMessage("MixinMessageHandler","Game Message: " + message.getString());
 
         MessageEvent.ReceiveMessageEvent event = new MessageEvent.ReceiveMessageEvent(message.getString());
         Pigeonqwq.EVENT_BUS.post(event).now();
