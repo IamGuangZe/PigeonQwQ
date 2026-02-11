2162fde01bde93ab7d5c9236ef22f4a8c13aecda
IamGuangZe <kstvGuangze@gmail.com>
2026-02-11 10:17:01 +0800

    feat: 为 VampireSlayer 添加游戏模式切换功能

--- src/main/java/owo/pigeon/modules/impl/Skyblock/Slayer/VampireSlayer.java
+++ src/main/java/owo/pigeon/modules/impl/Skyblock/Slayer/VampireSlayer.java
@@ -3,6 +3,7 @@ package owo.pigeon.modules.impl.Skyblock.Slayer;
 import net.engio.mbassy.listener.Handler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.world.GameMode;
 import owo.pigeon.event.events.TickEvent;
 import owo.pigeon.mixin.accessors.IAccessorInGameHud;
 import owo.pigeon.modules.Category;
@@ -22,15 +23,16 @@ import static owo.pigeon.Pigeon.mc;
 
 public class VampireSlayer extends Module {
     public VampireSlayer() {
-        super("VampireSlayer",Category.SKYBLOCK);
+        super("VampireSlayer", Category.SKYBLOCK);
     }
 
-    public EnableSetting autoHeal = setting("auto-heal",true,v->true);
+    public EnableSetting changeGamemode = setting("change-gamemode", true, v -> true);
+    public EnableSetting autoHeal = setting("auto-heal", true, v -> true);
     public FloatSetting healHealth = setting("heal-health", 6.0F, 1.0F, 20.0F, v -> autoHeal.getValue());
-    public EnableSetting autoIce = setting("auto-ice",true,v->true);
+    public EnableSetting autoIce = setting("auto-ice", true, v -> true);
     public IntSetting iceDelay = setting("ice-delay", 20, 0, 30, "tick", v -> autoIce.getValue());
     public EnableSetting autoSteak = setting("auto-steak", true, v -> true);
-    public EnableSetting autoImpel = setting("auto-impel", true,v->true);
+    public EnableSetting autoImpel = setting("auto-impel", true, v -> true);
     public EnableSetting autoImpelJump = setting("auto-impel-jump", true, v -> autoImpel.getValue());
     public EnableSetting autoImpelSneak = setting("auto-impel-sneak", true, v -> autoImpel.getValue());
     public EnableSetting autoImpelUp = setting("auto-impel-up", false, v -> autoImpel.getValue());
@@ -40,8 +42,8 @@ public class VampireSlayer extends Module {
         NONE, JUMP, SNEAK, UP, DOWN
     }
 
-    private boolean hasHeal,hasIced,hasImpel;
-    private int iceTicks,impelTicks;
+    private boolean hasHeal, hasIced, hasImpel;
+    private int iceTicks, impelTicks;
     private ImpelAction impelAction = ImpelAction.NONE;
     private float rawPitch;
 
@@ -62,13 +64,34 @@ public class VampireSlayer extends Module {
     @Handler
     public void onTickPost(TickEvent.ClientTickEvent.Post event) {
         if (WorldUtil.nullCheck()) return;
+        if (!SkyblockUtil.isInIsland(SkyblockUtil.Island.Rift)) return;
+
         Entity slayer = SkyblockUtil.getSlayer();
+
+        if (changeGamemode.getValue()) {
+            GameMode targetMode = (slayer != null) ? GameMode.ADVENTURE : GameMode.SURVIVAL;
+
+            if (mc.interactionManager != null && mc.interactionManager.getCurrentGameMode() != targetMode) {
+
+                boolean wasFlying = mc.player.getAbilities().flying;
+                boolean canFly = mc.player.getAbilities().allowFlying;
+
+                mc.interactionManager.setGameMode(targetMode);
+
+                mc.player.getAbilities().allowFlying = canFly;
+                mc.player.getAbilities().flying = wasFlying;
+
+                ChatUtil.sendCustomPrefixMessage(this.name, "Changed gamemode to &6" + targetMode.getId().toUpperCase());
+                ChatUtil.sendDebugMessage(this.name, "Client Gamemode -> " + targetMode.getId());
+            }
+        }
 
         if (autoHeal.getValue()) {
             int melon = ItemUtil.getSlotFromItemName(MELON);
             float health = mc.player.getHealth();
 
             if (melon != -1 && health <= healHealth.getValue() && !hasHeal) {
+                ChatUtil.sendDebugMessage(this.name, "Heal");
                 PlayerUtil.InstantUseItem(melon, PlayerUtil.RightClickMode.MOUSE);
                 hasHeal = true;
                 return;
@@ -82,8 +105,8 @@ public class VampireSlayer extends Module {
             if (ice == -1 || slayer == null) return;
 
             boolean foundClaws = false;
-            for (ArmorStandEntity stand : mc.world.getEntitiesByClass(ArmorStandEntity.class, slayer.getBoundingBox().expand(0.25,2.5,0.25), entity -> true)) {
-                ChatUtil.sendDebugMessage(this.name,"stand: " + stand.getName().getString());
+            for (ArmorStandEntity stand : mc.world.getEntitiesByClass(ArmorStandEntity.class, slayer.getBoundingBox().expand(0.25, 2.5, 0.25), entity -> true)) {
+                ChatUtil.sendDebugMessage(this.name, "stand: " + stand.getName().getString());
 
                 if (stand.getName().getString().contains("TWINCLAWS")) {
                     foundClaws = true;
@@ -96,6 +119,7 @@ public class VampireSlayer extends Module {
                     if (iceTicks < iceDelay.getValue()) {
                         iceTicks++;
                     } else {
+                        ChatUtil.sendDebugMessage(this.name, "ICE");
                         PlayerUtil.InstantUseItem(ice, PlayerUtil.RightClickMode.MOUSE);
                         hasIced = true;
                         iceTicks = 0;
@@ -112,7 +136,7 @@ public class VampireSlayer extends Module {
             int steak = ItemUtil.getSlotFromItemName(STEAK);
             if (steak == -1 || slayer == null) return;
 
-            for (ArmorStandEntity stand : mc.world.getEntitiesByClass(ArmorStandEntity.class, slayer.getBoundingBox().expand(0.25,2.5,0.25), entity -> true)) {
+            for (ArmorStandEntity stand : mc.world.getEntitiesByClass(ArmorStandEntity.class, slayer.getBoundingBox().expand(0.25, 2.5, 0.25), entity -> true)) {
                 if (stand.getName().getString().contains("҉") && stand.getName().getString().contains("Bloodfiend")) {
                     PlayerUtil.switchItemSlot(steak);
                 }
@@ -124,11 +148,12 @@ public class VampireSlayer extends Module {
     @Handler
     public void onTick(TickEvent.ClientTickEvent event) {
         if (WorldUtil.nullCheck()) return;
+        if (!SkyblockUtil.isInIsland(SkyblockUtil.Island.Rift)) return;
 
-        String subtitle = ((IAccessorInGameHud)mc.inGameHud).getSubtitle() == null ? "" : ColorUtil.removeColor(((IAccessorInGameHud)mc.inGameHud).getSubtitle().getString());
+        String subtitle = ((IAccessorInGameHud) mc.inGameHud).getSubtitle() == null ? "" : ColorUtil.removeColor(((IAccessorInGameHud) mc.inGameHud).getSubtitle().getString());
         boolean foundTitle = subtitle.startsWith("Impel: ");
 
-        if (!subtitle.isEmpty()) ChatUtil.sendDebugMessage(this.name,"subtitle: " + subtitle);
+        if (!subtitle.isEmpty()) ChatUtil.sendDebugMessage(this.name, "subtitle: " + subtitle);
 
         if (event instanceof TickEvent.ClientTickEvent.Pre) {
             if (autoImpel.getValue()) {
@@ -136,7 +161,8 @@ public class VampireSlayer extends Module {
                     if (subtitle.contains("JUMP") && autoImpelJump.getValue()) impelAction = ImpelAction.JUMP;
                     else if (subtitle.contains("SNEAK") && autoImpelSneak.getValue()) impelAction = ImpelAction.SNEAK;
                     else if (subtitle.contains("CLICK UP") && autoImpelUp.getValue()) impelAction = ImpelAction.UP;
-                    else if (subtitle.contains("CLICK DOWN") && autoImpelDown.getValue()) impelAction = ImpelAction.DOWN;
+                    else if (subtitle.contains("CLICK DOWN") && autoImpelDown.getValue())
+                        impelAction = ImpelAction.DOWN;
 
                     if (impelAction != ImpelAction.NONE) {
                         impelTicks = 5;
@@ -147,8 +173,8 @@ public class VampireSlayer extends Module {
 
                 if (impelTicks > 0) {
                     switch (impelAction) {
-                        case JUMP -> KeybindUtil.setPressed(mc.options.jumpKey,true);
-                        case SNEAK -> KeybindUtil.setPressed(mc.options.sneakKey,true);
+                        case JUMP -> KeybindUtil.setPressed(mc.options.jumpKey, true);
+                        case SNEAK -> KeybindUtil.setPressed(mc.options.sneakKey, true);
                         case UP -> {
                             mc.player.setPitch(-90f);
                             if (impelTicks % 2 != 0)
@@ -167,13 +193,13 @@ public class VampireSlayer extends Module {
         if (event instanceof TickEvent.ClientTickEvent.Post) {
             if (autoImpel.getValue()) {
                 if (impelTicks > 0) {
-                    impelTicks --;
+                    impelTicks--;
 
                     if (impelTicks <= 0) {
                         switch (impelAction) {
                             case JUMP -> KeybindUtil.resetPressed(mc.options.jumpKey);
                             case SNEAK -> KeybindUtil.resetPressed(mc.options.sneakKey);
-                            case UP,DOWN -> mc.player.setPitch(rawPitch);
+                            case UP, DOWN -> mc.player.setPitch(rawPitch);
                         }
 
                         impelAction = ImpelAction.NONE;
@@ -185,7 +211,7 @@ public class VampireSlayer extends Module {
         if (!foundTitle) {
 
             if (impelTicks != 0)
-                ChatUtil.sendDebugMessage(this.name,"reset impel.");
+                ChatUtil.sendDebugMessage(this.name, "reset impel.");
 
             hasImpel = false;
             impelTicks = 0;
