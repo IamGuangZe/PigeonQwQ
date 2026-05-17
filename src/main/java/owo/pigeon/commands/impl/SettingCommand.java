717d39b44f8287e353131908a4c8720809b69ae0
IamGuangZe <kstvGuangze@gmail.com>
2026-05-17 14:06:15 +0800

    feat: SettingCommand 支持按模块列出所有设置，可点击填充到聊天栏

--- src/main/java/owo/pigeon/commands/impl/SettingCommand.java
+++ src/main/java/owo/pigeon/commands/impl/SettingCommand.java
@@ -3,6 +3,10 @@ package owo.pigeon.commands.impl;
 import net.minecraft.block.Block;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.registry.Registries;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import owo.pigeon.commands.Command;
 import owo.pigeon.modules.Module;
@@ -19,9 +23,23 @@ public class SettingCommand extends Command {
         super("setting");
     }
 
-    // 无脑嵌套
     @Override
     public void execute(String[] args) {
+        if (args.length == 1) {
+            String modulename = args[0];
+            if (!ModuleUtil.isModuleExist(modulename)) {
+                CommandUtil.sendCommandError(CommandUtil.errorReason.UnknownModule,
+                        this.getCommand(),
+                        args,
+                        0
+                );
+                return;
+            }
+            Module module = ModuleUtil.getModule(modulename);
+            displayModuleSettings(module);
+            return;
+        }
+
         if (args.length < 3) {
             CommandUtil.sendCommandError(CommandUtil.errorReason.UnknownOrIncompleteCommand,
                     this.getCommand(),
@@ -289,9 +307,112 @@ public class SettingCommand extends Command {
         }
     }
 
+    private void displayModuleSettings(Module module) {
+        ChatUtil.sendMessage("&b" + module.name + " &8(" + module.getSettings().size() + " settings):");
+
+        for (AbstractSetting<?> setting : module.getSettings()) {
+            if (!setting.isVisible()) continue;
+
+            String cmdPrefix = String.valueOf(CommandUtil.getCommandPrefix());
+            String suggest = cmdPrefix + "setting " + module.name + " " + setting.getName() + " ";
+
+            MutableText line = Text.literal(ColorUtil.parseColor("&7 - " + setting.getName() + "&8: "))
+                    .append(setting instanceof ColorSetting s ? buildColorSettingText(s) : Text.literal(ColorUtil.parseColor(formatSettingValue(setting))));
+
+            line.styled(style -> style
+                    .withClickEvent(new ClickEvent.SuggestCommand(suggest))
+                    .withHoverEvent(new HoverEvent.ShowText(
+                            Text.literal(ColorUtil.parseColor(buildHoverText(setting)))
+                    ))
+            );
+            ChatUtil.sendMessage(line);
+        }
+    }
+
+    private MutableText buildColorSettingText(ColorSetting s) {
+        Color c = s.getValue();
+        int rgb = c.getRGB() & 0xFFFFFF;
+
+        MutableText hash = Text.literal("#").styled(style -> style.withColor(rgb));
+        MutableText r = Text.literal(ColorUtil.parseColor("&c" + String.format("%02X", c.getRed())));
+        MutableText g = Text.literal(ColorUtil.parseColor("&a" + String.format("%02X", c.getGreen())));
+        MutableText b = Text.literal(ColorUtil.parseColor("&9" + String.format("%02X", c.getBlue())));
+        MutableText a = Text.literal(ColorUtil.parseColor("&f" + String.format("%02X", c.getAlpha())));
+
+        return hash.append(r).append(g).append(b).append(a);
+    }
+
+    private String formatSettingValue(AbstractSetting<?> setting) {
+        if (setting instanceof EnableSetting s) return s.getValue() ? "&atrue" : "&cfalse";
+        else if (setting instanceof ExpandSetting s) return s.getValue() ? "&aexpand" : "&ccollapse";
+        else if (setting instanceof ListSetting s) return "&b[" + s.size() + " items]";
+        else if (setting instanceof ModeSetting<?> s) return "&b" + s.getValue().toString().toUpperCase();
+        else if (setting instanceof IntSetting s) {
+            String unit = s.getUnit();
+            String value = "&e" + s.getValue();
+            if (unit != null) value += " &7" + unit;
+            return value;
+        } else if (setting instanceof FloatSetting s) {
+            String unit = s.getUnit();
+            String value = "&e" + s.getValue();
+            if (unit != null) value += " &7" + unit;
+            return value;
+        } else if (setting instanceof StringSetting s) {
+            return "&7\"" + s.getValue().replace("&", "&&") + "&7\"";
+        } else if (setting instanceof CharSetting s) {
+            return "&7'" + String.valueOf(s.getValue()).replace("&", "&&") + "&7'";
+        } else if (setting instanceof KeySetting s) {
+            int code = s.getValue();
+            if (code == -1) return "&cNone";
+            try {
+                return InputUtil.Type.KEYSYM.createFromCode(code)
+                        .getTranslationKey().replace("key.keyboard.", "").toUpperCase();
+            } catch (Exception e) {
+                return String.valueOf(code);
+            }
+        } else if (setting instanceof BlockSetting s) {
+            return s.getValue().getName().getString();
+        }
+
+        return "&f" + setting.getValue().toString();
+    }
+
+    private String buildHoverText(AbstractSetting<?> setting) {
+        if (setting instanceof IntSetting s) {
+            String text = "&7Int&8: &f" + s.getMinValue() + " &8- &f" + s.getMaxValue();
+            if (s.getUnit() != null) text += " &8(&7" + s.getUnit() + "&8)";
+            return text;
+        } else if (setting instanceof FloatSetting s) {
+            String text = "&7Float&8: &f" + s.getMinValue() + " &8- &f" + s.getMaxValue();
+            if (s.getUnit() != null) text += " &8(&7" + s.getUnit() + "&8)";
+            return text;
+        } else if (setting instanceof ModeSetting<?> s) {
+            StringBuilder sb = new StringBuilder("&7Mode&8: ");
+            Enum<?>[] values = s.getValue().getDeclaringClass().getEnumConstants();
+            for (int i = 0; i < values.length; i++) {
+                if (i > 0) sb.append("&8, ");
+                sb.append("&f").append(values[i].toString());
+            }
+            return sb.toString();
+        } else if (setting instanceof ColorSetting s) {
+            Color c = s.getValue();
+            return "&7R&8:&f" + c.getRed() + " &7G&8:&f" + c.getGreen()
+                    + " &7B&8:&f" + c.getBlue() + " &7A&8:&f" + c.getAlpha();
+        } else if (setting instanceof ListSetting s) {
+            return "&7List &8(&f" + s.size() + " &7items&8)";
+        } else if (setting instanceof KeySetting s) {
+            return "&7Keybind";
+        } else if (setting instanceof EnableSetting) return "&7Enable";
+        else if (setting instanceof ExpandSetting) return "&7Expand";
+        else if (setting instanceof StringSetting) return "&7String";
+        else if (setting instanceof CharSetting) return "&7Character";
+        else if (setting instanceof BlockSetting) return "&7Block";
+        return "&7" + setting.getClass().getSimpleName();
+    }
+
     @Override
     public String getUsage() {
-        return CommandUtil.getCommandPrefix() + "setting <module> <setting> <value>\n" +
+        return CommandUtil.getCommandPrefix() + "setting <module> [<setting> <value>]\n" +
                 CommandUtil.getCommandPrefix() + "setting <module> <listsetting> (add|remove|list) <value>";
     }
 }
